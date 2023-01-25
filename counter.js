9614a4590b7f749a5f32af0e7b781cd72dfa9519
Tugba Dogan <104082640+thelastwaterdrinker@users.noreply.github.com>
2023-01-25 10:46:27 +0000

    Add files via upload

--- counter.js
+++ counter.js
@@ -1,8 +1,12 @@
-let counter = 0;
-
+//We first check if there is any value in local storage for counter, and then if there is not set it to 0.
+if(!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 function count() {
+    let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter); //Set the new value to local storage value
 
     if (counter %10 === 0) {
         alert(`Count is now ${counter}`);
@@ -10,5 +14,6 @@ function count() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
 });
